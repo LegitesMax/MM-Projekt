8370966f9aa6867b14051c5b94df49f36b0a35d4
LegitesMax <55409186+LegitesMax@users.noreply.github.com>
2025-05-11 14:55:48 +0200

    add ASP Controller base

---
+++ TCC/TCC.Asp/Controller/GenericController.cs
@@ -0,0 +1,21 @@
+﻿namespace TCC.Asp.Controller
+{
+    public abstract class GenericController<TAccesModel, TViewModel> 
+        where TViewModel : class, new() 
+        where TAccesModel : class, new()
+    {
+        protected virtual TAccesModel ToTAccesModel(TViewModel viewModel)
+        {
+            var result = new TAccesModel();
+
+            throw new NotImplementedException();
+        }
+
+        protected virtual TViewModel ToViewModel(TViewModel viewModel)
+        {
+            var result = new TViewModel();
+
+            throw new NotImplementedException();
+        }
+    }
+}
