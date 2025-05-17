8f75d93515c03692c161f18374feebf807880710
LegitesMax <55409186+LegitesMax@users.noreply.github.com>
2025-05-17 15:16:19 +0200

    add working Controller and add Selectlists

--- TCC/TCC.Asp2/Controllers/HomeController.cs
+++ TCC/TCC.Asp2/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using TCC.Asp.Models;
 using TCC.Asp2.Models;
+using TCC.Logic.Base;
+using TCC.Logic.Implementations.Enums;
 
 namespace TCC.Asp2.Controllers
 {
@@ -21,6 +24,28 @@ namespace TCC.Asp2.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult BtnCompressText(string input, CompressionAlgorithms compressionType)
+        {
+            var result = new AlgorithmResult();
+
+            var compressionMethod = SelectCompressionModel(compressionType);
+
+            result.Output = compressionMethod.ComputeOutput(input).Output;
+            result.Input = compressionMethod.ComputeOutput(input).Input;
+
+            return View("Index", result);
+        }
+
+        public BaseApplicableAlgorithm SelectCompressionModel(CompressionAlgorithms compressionType)
+        {
+            return compressionType switch
+            {
+                CompressionAlgorithms.Huffman => new Logic.Implementations.HuffmanAlgorithmImpl(),
+                //Other Compress. Types....
+                _ => throw new NotSupportedException($"Algorithmus {compressionType} wird nicht unterstützt.")
+            };
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ TCC/TCC.Asp2/Models/CompressModel.cs
@@ -0,0 +1,12 @@
+﻿using TCC.Logic.Base;
+
+namespace TCC.Asp.Models
+{
+    public class CompressModel
+    {
+        public string Input { get; set; } = string.Empty;
+        public string Output { get; set; } = string.Empty;
+
+        public InputOutputStatistic Statistic => new();
+    }
+}
